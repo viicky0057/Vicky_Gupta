cc51b317218a6e87039706b8e1edf18a9f9ac31d
Vicky <97652847+Vickykr369@users.noreply.github.com>
2022-11-08 00:18:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let body = document.getElementById("body");
+let elm = document.createElement("div")
+elm.id = "cls"
+let time = document.createElement("span")
+time.id = "time"
+let locations = document.createElement("span")
+time.id = "loc"
+let m = Math
+
+let mm = document.getElementById("mm");
+let message = document.createElement("span");
+message.id = "msg"
+message.innerText = "Recording info.. "
+setInterval(() => {
+    elm.innerText = ((m.floor(m.random() * 100000000000000000000 + 999999999999)));
+    time.innerText = new Date
+
+    elm.appendChild(time)
+    elm.appendChild(locations)
+    locations.innerText = "   Near Gopalganj Bihar";
+    elm.appendChild(message)
+}, 200);
+
+document.addEventListener("click", () => {
+    mm.style.display = "none"
+    body.appendChild(elm);
+    setTimeout(() => {
+        message.innerText = "Data recorded "
+    }, 2000);
+    setTimeout(() => {
+        message.innerText = "Sent "
+    }, 4000);
+
+    // setInterval(() => {
+    //     alert("This site is stealing your data")
+    // }, 20000);
+})
